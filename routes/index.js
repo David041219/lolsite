18d8083c01015b96c283886737221e1e26a112a4
David041219 <neverdie1219@gmail.com>
2019-05-19 13:19:29 +0900

    롤 업데이

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/getdata', function(req, res, next) {
+console.log('가져옴', req.body);  
+return res.json({
+  'searchid': req.body.searchid,
+  'data': '벌레다'
+});
 });
 
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('main', { title: '롤 전적검색' });
+});
 module.exports = router;
+
